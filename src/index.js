e56e976d8d12ffdba3503f039db66d6c42d1426b
joaolucasdemelo <joaolucas.melo1619@gmail.com>
2024-01-04 00:54:52 -0300

    projeto concluido

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 class Pirata {
     constructor(nome, idade) {
-      this.nome = nome;
-      this.idade = idade;
+      this.nome = "Luffy";
+      this.idade = 19;
       this.tipo = "pirata";
     }
   
     atacar() {
-      console.log(`O pirata ${this.nome} atacou usando sabre`);
+      console.log(`O pirata ${this.nome} atacou usando gomu gomu no rifle`);
     }
   }
   
   // Exemplo de uso da classe Pirata
-  const pirata = new Pirata("Barba Negra", 45);
+  const pirata = new Pirata("Monkey D. Luffy", 19);
   
   pirata.atacar();
